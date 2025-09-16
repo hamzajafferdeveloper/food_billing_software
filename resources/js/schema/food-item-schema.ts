cc8fdd82c523dff1a4a6d7a7d644aebec3cd0191
hamzajafferdeveloper <hamzajaffer999@gmail.com>
2025-09-16 11:33:51 +0500

    Implement Form Image Max Validation in Food Item & Category

--- resources/js/schema/food-item-schema.ts
+++ resources/js/schema/food-item-schema.ts
@@ -7,8 +7,18 @@ export const foodItemSchema = z.object({
     image: z
         .instanceof(File)
         .optional()
-        .refine((file) => {
-            if (!file) return true;
-            return ['image/jpeg', 'image/png', 'image/jpg', 'image/gif', 'image/svg+xml'].includes(file.type);
-        }, 'Invalid file type'),
+        .refine(
+            (file) => {
+                if (!file) return true;
+                return ['image/jpeg', 'image/png', 'image/jpg', 'image/gif', 'image/svg+xml'].includes(file.type);
+            },
+            { message: 'Invalid file type' },
+        )
+        .refine(
+            (file) => {
+                if (!file) return true;
+                return file.size <= 2 * 1024 * 1024; // 2MB
+            },
+            { message: 'File must be less than 2MB' },
+        ),
 });
